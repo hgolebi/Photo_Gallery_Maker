2086be6e153e679db654f2db5a92086047c34335
Hubert <hugo7744@gmail.com>
2021-06-03 21:04:07 +0200

    photos are now displayed one by one

--- gui.py
+++ gui.py
@@ -8,6 +8,7 @@ import sys
 
 url = 'https://source.unsplash.com/featured/?{KEYWORD}'
 
+
 class MainWindow(QtWidgets.QMainWindow):
     def __init__(self, parent=None):
         super().__init__(parent)
@@ -22,12 +23,10 @@ class MainWindow(QtWidgets.QMainWindow):
             self.ui.img5,
             self.ui.img6,
         ]
+        self.ui._displayedImg = None
         self.ui.enterButton.clicked.connect(lambda: self._search())
-
-    # def _getPhoto(self, keyword):
-    #     response = requests.get(url.format(KEYWORD=keyword))
-    #     img_bytes = BytesIO(response.content)
-    #     return img_bytes
+        self.ui.leftButton.clicked.connect(lambda: self._swipeLeft())
+        self.ui.rightButton.clicked.connect(lambda: self._swipeRight())
 
     def _search(self):
         text = self.ui.writeKeyword.text()
@@ -40,9 +39,29 @@ class MainWindow(QtWidgets.QMainWindow):
                 response = requests.get(url.format(KEYWORD=text))
             img_bytes = BytesIO(response.content)
             pixmapa.loadFromData(img_bytes.getvalue())
+            height = pixmapa.height()
+            pixmapa = pixmapa.scaledToHeight(min(840, height))
             img.setPixmap(pixmapa)
             prev_response_url = response.url
-        self.ui.stackedWidget.setCurrentIndex(1)
+        self.ui.imgStack.setCurrentIndex(0)
+        self.ui._displayedImg = 1
+        self.ui.stackedWidget.setCurrentIndex(2)
+
+    def _swipeLeft(self):
+        if self.ui._displayedImg == 1:
+            return
+        else:
+            current_page = self.ui._displayedImg - 1
+            self.ui.imgStack.setCurrentIndex(current_page - 1)
+            self.ui._displayedImg -= 1
+
+    def _swipeRight(self):
+        if self.ui._displayedImg == 6:
+            return
+        else:
+            current_page = self.ui._displayedImg - 1
+            self.ui.imgStack.setCurrentIndex(current_page + 1)
+            self.ui._displayedImg += 1
 
 
 def guiMain(args):
@@ -50,6 +69,7 @@ def guiMain(args):
     window = MainWindow()
     window.show()
     return app.exec_()
+
 
 if __name__ == "__main__":
     guiMain(sys.argv)
